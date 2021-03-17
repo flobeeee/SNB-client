ec43adf490359d2ebf719077fa96ec1db3d9f101
Pangho297 <monanazzang@gmail.com>
2021-03-17 17:43:13 +0900

    fix App.js

--- snb-client/src/App.js
+++ snb-client/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [accessToken, setAccessToken] = useState(null);
 
   const getAccessToken = async (authorizationCode) => {
-    let res = await axios.post('https://localhost:4000/oauth/login', { authorizationCode });
+    let res = await axios.post('https://songnumberbook.ga:4000/oauth/login', { authorizationCode });
 
     setAccessToken(res.data.accessToken);
     login();
