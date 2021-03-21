556ba8fc061317e090acb920988cd8bf4a91b516
kimyoosang <user@gim-yusang-ui-MacBookPro.local>
2021-03-22 05:16:07 +0900

    Fix all error...

--- snb-client/src/components/AddSong.js
+++ snb-client/src/components/AddSong.js
@@ -5,7 +5,7 @@ import './Addsong.css';
 import Modal from '../components/modal/CenterModal';
 const AddSong = ({ userdata, songList }) => {
 
-  const [value, setValue] = useState(userdata.lists[0].id);
+  const [value, setValue] = useState(userdata.lists.length !== 0 ? userdata.lists[0].id : []);
   const result = songList.reduce((a, b) => {
     if (a.indexOf(b) < 0) { a.push(b); }
     return a;
@@ -51,15 +51,16 @@ const AddSong = ({ userdata, songList }) => {
     setValue(e.target.value);
   };
 
+  console.log('유저데이터', userdata.lists);
   return (
     <div className='addsong'>
       <Modal visible={isOpenPopup} color={'#aea1ea'} isBlackBtn={true} onClose={closePopUp} backColor={true}>
         {isAddBtn ? (<div>항목을 선택해 주세요</div>) : (<div>저장되었습니다</div>)}
       </Modal>
-      <select className='listDropdown' name="list" id="listDropdown" value={value} onChange={handleChange}>
-        {userdata.lists.map(list => {
-          return <option className='option' key={list.id} value={list.id}>{list.name}</option>;
-        })}
+      <select className='listDropdown' name="list" id="listDropdown" onChange={handleChange}>
+        {userdata.lists ? userdata.lists.map(list => {
+          return <option key={list.id} className='option'>{list.name}</option>;
+        }) : (<option className='option'>리스트 없음</option>)}
       </select>
       <button className="search-aaddlistbtn" onClick={handleClick}>내 리스트에 저장</button>
     </div>
