87b54d6c1bc4d06dbea3ddab9f4d7d0b01871a89
Pangho297 <monanazzang@gmail.com>
2021-03-21 16:03:46 +0900

    fix Search.css and Song.css

--- snb-client/src/pages/Search.js
+++ snb-client/src/pages/Search.js
@@ -102,8 +102,7 @@ const Search = ({ searchValue, searchType, title, userdata, isNext, nowPages }) 
         <div className='info-num'>번호</div>
         <div className='info-title'>제목</div>
         <div className='info-singer'>가수</div>
-        <div className='info-media'>미디어</div>
-        <div className='info-check'>선택</div>
+        <div className='info-media'>미디어 / 선택</div>
       </div>
       <div className='songs'>
         {result.results.map((data) => (
