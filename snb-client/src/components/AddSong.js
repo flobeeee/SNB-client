1bfb36d8bf8da82630a046f16e28756be2e52dd5
Pangho297 <monanazzang@gmail.com>
2021-03-22 14:34:24 +0900

    fix AddSong.js, AddSong.css

--- snb-client/src/components/AddSong.js
+++ snb-client/src/components/AddSong.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import './Addsong.css';
 import Modal from '../components/modal/CenterModal';
+import Select from 'react-select';
+
 const AddSong = ({ userdata, songList }) => {
 
-  const [value, setValue] = useState(userdata.lists[0].id);
+  const [isSearchable] = useState(false);
+  const [listId, setListId] = useState(userdata.lists[0].id);
   const result = songList.reduce((a, b) => {
     if (a.indexOf(b) < 0) { a.push(b); }
     return a;
@@ -14,7 +17,9 @@ const AddSong = ({ userdata, songList }) => {
   const [isOpenPopup, setIsOpenPopup] = useState(false);
   const [isAddBtn, setIsAddBtn] = useState(true);
 
-
+  const options = userdata.lists.map((list) => {
+    return { value: list.id, label: list.name };
+  });
 
   const isAdd = (e) => {
     if (e === true) {
@@ -36,7 +41,7 @@ const AddSong = ({ userdata, songList }) => {
 
   const handleClick = async () => {
     await axios.post('https://localhost:4000/mylist/song/add',
-      { listid: value, songs: result }, { withCredentials: true })
+      { listid: listId, songs: result }, { withCredentials: true })
       .then(res => {
         if (result.length === 0) {
           isAdd(true);
@@ -47,8 +52,24 @@ const AddSong = ({ userdata, songList }) => {
       });
   };
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
+  const handleChange = (seletedListId) => {
+    setListId(seletedListId.value);
+  };
+
+  const customStyles = {
+    input: () => ({
+      outline: 'none'
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px solid #8D44AD',
+      color: state.isSelected ? '#e8447d' : '#646464',
+      background: 'none',
+      padding: 10
+    }),
+    singleValue: () => ({
+      color: '#646464'
+    })
   };
 
   return (
@@ -56,11 +77,16 @@ const AddSong = ({ userdata, songList }) => {
       <Modal visible={isOpenPopup} color={'#aea1ea'} isBlackBtn={true} onClose={closePopUp} backColor={true}>
         {isAddBtn ? (<div>항목을 선택해 주세요</div>) : (<div>저장되었습니다</div>)}
       </Modal>
-      <select className='listDropdown' name="list" id="listDropdown" value={value} onChange={handleChange}>
-        {userdata.lists.map(list => {
-          return <option className='option' key={list.id} value={list.id}>{list.name}</option>;
-        })}
-      </select>
+      <Select
+        className="dropdown"
+        styles={customStyles}
+        placeholder="리스트를 선택해주세요"
+        value={options.label}
+        onChange={handleChange}
+        options={options}
+        isSearchable={isSearchable}
+        menuPlacement="top"
+      />
       <button className="search-aaddlistbtn" onClick={handleClick}>내 리스트에 저장</button>
     </div>
 
