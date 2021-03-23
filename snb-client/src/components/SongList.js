a8a7d8f61e19c17072444bcf6902a62c2276568e
EunJeong Lee <sssver28@gmail.com>
2021-03-23 09:44:35 +0900

    fix SongList.css

--- snb-client/src/components/SongList.js
+++ snb-client/src/components/SongList.js
@@ -42,16 +42,18 @@ const SongList = ({ songs, listId, setSongs }) => {
           <div className="info-singer">가수</div>
           <div className="info-media">미디어 / 선택</div>
         </div>
-        {songs ? (songs.map((data) =>
-          <Song
-            key={data.songNum}
-            songNum={String(data.songNum)}
-            title={data.title}
-            singer={data.singer}
-            link={data.link}
-            getSongs={getSongs}
-          />))
-          : <div></div>}
+        <div className="songs-box">
+          {songs ? (songs.map((data) =>
+            <Song
+              key={data.songNum}
+              songNum={String(data.songNum)}
+              title={data.title}
+              singer={data.singer}
+              link={data.link}
+              getSongs={getSongs}
+            />))
+            : <div></div>}
+        </div>
         <div className="userinfo-removebox">
           <div className="userinfo-seletsong">{checkedSongList?.length}/{songs?.length}</div>
           <button className="userinfo-removesong" onClick={requestRemoveSong}>삭제</button>
